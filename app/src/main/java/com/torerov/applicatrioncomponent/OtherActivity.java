0e533d440d58536bd291800de91bc0ebc38912e2
choijaewoong <jwchoi0129@gmail.com>
2015-10-07 11:52:02 +0900

    StartActivityForResult

--- app/src/main/java/com/torerov/applicatrioncomponent/OtherActivity.java
+++ app/src/main/java/com/torerov/applicatrioncomponent/OtherActivity.java
@@ -1,5 +1,6 @@
 package com.torerov.applicatrioncomponent;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +11,9 @@ import android.widget.TextView;
 public class OtherActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "message";
+    public static final String RESULT_MESSAGE = "message";
     TextView messageView;
+    String mMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,15 @@ public class OtherActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra(RESULT_MESSAGE, "echo : " + mMessage);
+                setResult(Activity.RESULT_OK, intent);
                 finish();
             }
         });
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(EXTRA_MESSAGE);
-        messageView.setText(message);
+        mMessage = intent.getStringExtra(EXTRA_MESSAGE);
+        messageView.setText(mMessage);
     }
 }
